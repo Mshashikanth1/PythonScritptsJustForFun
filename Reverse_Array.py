ab7985c87850f489c117107fe6c6c7a9747c831b
shashikanth <shashikanthvarma98@gmail.com>
2021-06-24 22:18:52 +0530

    Create Reverse_Array.py

---
+++ Reverse_Array.py
@@ -0,0 +1,17 @@
+
+def reverse(arr,start,end):
+    while start<end:
+          arr[start],arr[en>
+          start+=1
+          end-=1
+def reverse_rec(arr,start,e>
+    if start<end:
+       arr[start],arr[end]=>
+       reverse_rec(arr,star>
+
+
+#driver code
+a=list(map(int,input().spli>
+#reverse(a,0,len(a)-1)
+reverse(a,0,len(a)-1)
+print(a)
